da9ee2b545e58230312abc61164169f7223ac722
kasiakowalska <kasia,97.kowalska@gmail.com>
2019-10-24 18:45:58 +0200

    a

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.io.PrintStream;
+import java.math.BigInteger;
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+public class Calculator {
+    public Calculator(){
+    }
+
+    public BigInteger add(BigInteger a, BigInteger b){
+        return a.add(b);
+    }
+}
