7cfa81fbe6c22dd2f42636851c335c6487faede5
mrtplk <martapalka13@gmail.com>
2019-10-24 19:25:21 +0200

    zad1 zad2 - bez menu

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -5,10 +5,19 @@ import java.util.Scanner;
 import static java.lang.Math.*;
 
 public class Calculator {
+
+
     public Calculator(){
     }
 
     public BigInteger add(BigInteger a, BigInteger b){
         return a.add(b);
     }
+    public BigInteger sub(BigInteger a, BigInteger b) { return a.subtract(b); }
+    public BigInteger mul(BigInteger a, BigInteger b){
+        return a.multiply(b);
+    }
+    public BigInteger mod(BigInteger a, BigInteger b) { return a.modInverse(b); }
+
+    //~Calculator();
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,28 +2,96 @@ package com.company;
 
 import java.io.PrintStream;
 import java.math.BigInteger;
+import java.util.Random;
 import java.util.Scanner;
 import static java.lang.Math.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        Random rand = new Random();
+        BigInteger b1 = new BigInteger(1024, rand); // (2^4-1) = 15 is the maximum value
+        //BigInteger b2 = new BigInteger(1024, rand);
+        //BigInteger b2 = new BigInteger("3");
+        BigInteger b2 =BigInteger.probablePrime(1024, rand);
+
         System.out.println("Wpisz mała liczbe nr 1");
         BigInteger l1 = scan.nextBigInteger();
         System.out.println("Wpisz mała liczbe nr 2");
         BigInteger l2 = scan.nextBigInteger();
 
-        BigInteger sum = l1.add(l2);
-        BigInteger sub = l1.subtract(l2);
-        BigInteger mul= l1.multiply(l2);
-        BigInteger mod = l1.modInverse(l2);
+        BigInteger a =calculator.add(l1,l2);
+        BigInteger b =calculator.sub(l1,l2);
+        BigInteger c =calculator.mul(l1,l2);
+        BigInteger d =calculator.mod(l1,l2);
 
-        System.out.println("Wynik dodawania: " + sum);
-        System.out.println("Wynik odejmowania: " + sub);
-        System.out.println("Wynik mnożenia: " + mul);
-        System.out.println("Liczenie odwrotności elementu ciała (liczby) modulo duża liczba pierwsza p: " + mod);
+        BigInteger e =calculator.add(b1,b2);
+        BigInteger f =calculator.sub(b1,b2);
+        BigInteger g =calculator.mul(b1,b2);
+        BigInteger h =calculator.mod(b1,b2);
+        /*
+        int wybor = menu();
 
-        BigInteger bigL = BigInteger(1024,)
+        switch(wybor){
+            case 1:
+                int a, b;
+                System.out.println("Podaj dwie liczby do zsumowania");
+                a = in.nextInt();
+                b = in.nextInt();
+
+                int wynik = suma(a,b);
+
+                System.out.format("Suma liczb wynosi "+wynik);
+
+                break;
+
+            case 2:
+                System.out.println("Podaj liczbę do obliczenia sinusa");
+                a = in.nextInt();
+
+                double wynik2 = sinus(a);
+
+                System.out.format("Sinus wynosi "+wynik2);
+
+                break;
+
+            case 3:
+                informacja();
+
+        }
+        */
+        System.out.println("Wynik dodawania: " + a);
+        System.out.println("Wynik odejmowania: " + b);
+        System.out.println("Wynik mnożenia: " + c);
+        System.out.println("Liczenie odwrotności elementu ciała (liczby) modulo duża liczba pierwsza p: " + d);
+
+        System.out.println("Duza liczba nr 1: " + b1);
+        System.out.println("Duza liczba nr 2: " + b2);
+        System.out.println("Wynik dodawania: " + e);
+        System.out.println("Wynik odejmowania: " + f);
+        System.out.println("Wynik mnożenia: " + g);
+        System.out.println("Liczenie odwrotności elementu ciała (liczby) modulo duża liczba pierwsza p: " + h);
+
+
+    }
+
+    public static int menu(){
+        System.out.println();
+        System.out.println("     ****************************************");
+        System.out.println("     *                 MENU                 *");
+        System.out.println("     ****************************************");
+        System.out.println("     1. Suma");
+        System.out.println("     2. Sinus");
+        System.out.println("     3. Informaja");
+        System.out.println("     0. Koniec");
+
+        Scanner in = new Scanner(System.in);
+        int w = in.nextInt();
+
+        return w;
     }
 }
