788f4c6982f2e3e5af05dc43d3858e9894eed30e
kasiakowalska <kasia,97.kowalska@gmail.com>
2019-10-24 18:46:18 +0200

    a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,10 +13,17 @@ public class Main {
         BigInteger l1 = scan.nextBigInteger();
         System.out.println("Wpisz mała liczbe nr 2");
         BigInteger l2 = scan.nextBigInteger();
+
         BigInteger sum = l1.add(l2);
-        System.out.print("Wynik dodawania: ");
-        System.out.println(sum);
+        BigInteger sub = l1.subtract(l2);
+        BigInteger mul= l1.multiply(l2);
+        BigInteger mod = l1.modInverse(l2);
 
+        System.out.println("Wynik dodawania: " + sum);
+        System.out.println("Wynik odejmowania: " + sub);
+        System.out.println("Wynik mnożenia: " + mul);
+        System.out.println("Liczenie odwrotności elementu ciała (liczby) modulo duża liczba pierwsza p: " + mod);
 
+        BigInteger bigL = BigInteger(1024,)
     }
 }
