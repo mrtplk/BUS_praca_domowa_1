25246dba14b44a413a2533c4189f54fb5675d6ae
mrtplk <martapalka13@gmail.com>
2019-10-24 18:10:57 +0200

    Add-small numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,22 @@
 package com.company;
 
 import java.io.PrintStream;
+import java.math.BigInteger;
+import java.util.Scanner;
+import static java.lang.Math.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Wpisz mała liczbe nr 1");
+        BigInteger l1 = scan.nextBigInteger();
+        System.out.println("Wpisz mała liczbe nr 2");
+        BigInteger l2 = scan.nextBigInteger();
+        BigInteger sum = l1.add(l2);
+        System.out.print("Wynik dodawania: ");
+        System.out.println(sum);
+
+
     }
 }
