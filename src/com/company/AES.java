da29f4e5a3fd5b7ed3c1568ae4d6e54ce5c4e6bc
mrtplk <martapalka13@gmail.com>
2019-10-26 15:10:37 +0200

    DZIALA

---
+++ src/com/company/AES.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+public class AES {
+    private static SecretKeySpec secretKey; //1
+    private static byte[] key;
+
+    public static void setKey(String myKey)
+    {
+        MessageDigest sha = null; //2
+        try {
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-1"); //3 - Returns a MessageDigest object that implements the specified digest algorithm
+            key = sha.digest(key); //4 - Performs a final update on the digest using the specified array of bytes, then completes the digest computation.
+            key = Arrays.copyOf(key, 16); //It copies the specified array, truncating or padding with false (if necessary) so the copy has the specified length.
+            secretKey = new SecretKeySpec(key, "AES"); //5- Constructs a secret key from the given byte array.
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String encrypt(String strToEncrypt, String secret)
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding"); //This class provides the functionality of a cryptographic cipher for encryption and decryption.
+            //In order to create a Cipher object, the application calls the Cipher's getInstance method, and passes the name of the requested transformation to it. Optionally, the name of a provider may be specified.
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8"))); //static methods for obtaining encoders and decoders for the Base64 encoding scheme.
+            //Finishes a multiple-part encryption or decryption operation, depending on how this cipher was initialized. - doFinal
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    public static String decrypt(String strToDecrypt, String secret)
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import java.io.PrintStream;
+import java.io.*;
 import java.math.BigInteger;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -14,81 +14,81 @@ import static java.lang.Math.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException {
+    public static void main(String[] args) throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, IOException {
 
         Scanner scan = new Scanner(System.in);
         Calculator calculator = new Calculator();
         Random rand = new Random();
-        BigInteger b1 = new BigInteger(1024, rand); // (2^4-1) = 15 is the maximum value
-        //BigInteger b2 = new BigInteger(1024, rand);
-        //BigInteger b2 = new BigInteger("3");
-        BigInteger b2 =BigInteger.probablePrime(1024, rand);
 
-        Encryption ee = new Encryption();
-        byte[] e1 = ee.encrypt();
-        ee.decrypt(e1);
-        System.out.println("Wpisz mała liczbe nr 1");
-        BigInteger l1 = scan.nextBigInteger();
-        System.out.println("Wpisz mała liczbe nr 2");
-        BigInteger l2 = scan.nextBigInteger();
+        Boolean carryOn = true;
 
-        BigInteger a =calculator.add(l1,l2);
-        BigInteger b =calculator.sub(l1,l2);
-        BigInteger c =calculator.mul(l1,l2);
-        BigInteger d =calculator.mod(l1,l2);
+        while(carryOn) {
+            int choice = menu();
+            switch (choice) {
+                case 0:
+                    carryOn=false;
 
-        BigInteger e =calculator.add(b1,b2);
-        BigInteger f =calculator.sub(b1,b2);
-        BigInteger g =calculator.mul(b1,b2);
-        BigInteger h =calculator.mod(b1,b2);
+                    break;
+                case 1:
+                    System.out.println("Wpisz mała liczbe nr 1");
+                    BigInteger l1 = scan.nextBigInteger();
+                    System.out.println("Wpisz mała liczbe nr 2");
+                    BigInteger l2 = scan.nextBigInteger();
 
-        //
-        //ee.getPublicKey();
+                    BigInteger a = calculator.add(l1, l2);
+                    BigInteger b = calculator.sub(l1, l2);
+                    BigInteger c = calculator.mul(l1, l2);
+                    BigInteger d = calculator.mod(l1, l2);
 
-        /*
-        int wybor = menu();
+                    System.out.println("Wynik dodawania: " + a);
+                    System.out.println("Wynik odejmowania: " + b);
+                    System.out.println("Wynik mnożenia: " + c);
+                    System.out.println("Liczenie odwrotności elementu ciała (liczby) modulo duża liczba pierwsza p: " + d);
 
-        switch(wybor){
-            case 1:
-                int a, b;
-                System.out.println("Podaj dwie liczby do zsumowania");
-                a = in.nextInt();
-                b = in.nextInt();
+                    break;
 
-                int wynik = suma(a,b);
+                case 2:
+                    BigInteger b1 = new BigInteger(1024, rand); // (2^4-1) = 15 is the maximum value
+                    BigInteger b2 = BigInteger.probablePrime(1024, rand);
 
-                System.out.format("Suma liczb wynosi "+wynik);
+                    BigInteger e = calculator.add(b1, b2);
+                    BigInteger f = calculator.sub(b1, b2);
+                    BigInteger g = calculator.mul(b1, b2);
+                    BigInteger h = calculator.mod(b1, b2);
 
-                break;
+                    System.out.println("Duza liczba nr 1: " + b1);
+                    System.out.println("Duza liczba nr 2: " + b2);
+                    System.out.println("Wynik dodawania: " + e);
+                    System.out.println("Wynik odejmowania: " + f);
+                    System.out.println("Wynik mnożenia: " + g);
+                    System.out.println("Liczenie odwrotności elementu ciała (liczby) modulo duża liczba pierwsza p: " + h);
 
-            case 2:
-                System.out.println("Podaj liczbę do obliczenia sinusa");
-                a = in.nextInt();
+                    break;
 
-                double wynik2 = sinus(a);
+                case 3:
+                    //Encryption ee = new Encryption();
+                    //byte[] e1 = ee.encrypt();
+                    //ee.decrypt(e1);
+                    final String secretKey = "ssshhhhhhhhhhh!!!!";
 
-                System.out.format("Sinus wynosi "+wynik2);
 
-                break;
+                    String originalString = readingFromFile("/Users/marta/IdeaProjects/BUS_praca_domowa_1/src/com/company/message.txt");
+                    System.out.print(originalString);
 
-            case 3:
-                informacja();
+                    String encryptedString = AES.encrypt(originalString, secretKey) ;
+                    String decryptedString = AES.decrypt(encryptedString, secretKey) ;
 
+                    //System.out.println(originalString);
+                    System.out.println("Zaszyforwane dane: " + encryptedString);
+                    System.out.println("Odszyfrowane dane " + decryptedString);
+                    carryOn=false;
+                    break;
+                case 4:
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + choice);
+            }
         }
-        */
-        System.out.println("Wynik dodawania: " + a);
-        System.out.println("Wynik odejmowania: " + b);
-        System.out.println("Wynik mnożenia: " + c);
-        System.out.println("Liczenie odwrotności elementu ciała (liczby) modulo duża liczba pierwsza p: " + d);
-
-        System.out.println("Duza liczba nr 1: " + b1);
-        System.out.println("Duza liczba nr 2: " + b2);
-        System.out.println("Wynik dodawania: " + e);
-        System.out.println("Wynik odejmowania: " + f);
-        System.out.println("Wynik mnożenia: " + g);
-        System.out.println("Liczenie odwrotności elementu ciała (liczby) modulo duża liczba pierwsza p: " + h);
-
-
     }
 
     public static int menu(){
@@ -96,9 +96,10 @@ public class Main {
         System.out.println("     ****************************************");
         System.out.println("     *                 MENU                 *");
         System.out.println("     ****************************************");
-        System.out.println("     1. Suma");
-        System.out.println("     2. Sinus");
-        System.out.println("     3. Informaja");
+        System.out.println("     1. Zad 1");
+        System.out.println("     2. Zad 2");
+        System.out.println("     3. Zad 3");
+        System.out.println("     4. Zad 4");
         System.out.println("     0. Koniec");
 
         Scanner in = new Scanner(System.in);
@@ -106,4 +107,18 @@ public class Main {
 
         return w;
     }
+    public static String readingFromFile(String path) throws IOException {
+        FileInputStream file = new FileInputStream(path);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(file));
+        StringBuilder str = new StringBuilder();
+        String line = bf.readLine();
+        while (line != null){
+            str.append(line);
+            str.append(System.lineSeparator());
+            line = bf.readLine();
+        }
+        String result = str.toString();
+        return result;
+    }
+
 }
